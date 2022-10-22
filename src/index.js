4e5c8d4fb121a21747ea4f9e91eb17e56cda2a79
cvine <rafarakh@gmail.com>
2022-10-22 16:53:25 +0300

    feat: random character info renders in the top of the page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
-import MarvelSrevice from "./services/MarvelService";
 
 import "./style/style.scss";
-
-const marvelService = new MarvelSrevice();
-
-marvelService.getAllCharacters().then((res) =>
-	res.data.results.forEach((element) => {
-		console.log(element.name);
-	})
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
